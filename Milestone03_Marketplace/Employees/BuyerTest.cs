de1cfadaf7f3710a21e9314ab30a6de1ebafed05
Amy Dayasundara <a.dayasundara@gmail.com>
2019-11-23 10:48:35 -0500

    :bento: Services partial start

---
+++ Milestone03_Marketplace/Employees/BuyerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Milestone03_Marketplace.Employees
+{
+    [TestClass]
+    public class BuyerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
